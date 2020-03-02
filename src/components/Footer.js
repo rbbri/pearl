b4021e8ba484f250441eee471a998d60c01761ea
Robin Johnson <robin.s.johnson@gmail.com>
2020-03-02 13:39:32 +0000

    Adds image and subtitle to about - Remove social links form footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import logo from '../img/logo.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
 
 const Footer = class extends React.Component {
   render() {
@@ -21,7 +18,7 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
             <div className="columns">
-              <div className="column is-4 social">
+              <div className="column is-2 social">
                 <a title="facebook" href="https://facebook.com">
                   <img
                     src={facebook}
