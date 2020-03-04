f7b954249fcf43df2cc294cab513ee63b0501e44
Robin Johnson <robin.s.johnson@gmail.com>
2020-03-04 15:23:06 +0000

    Adds cms for about

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -2,14 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { IndexPageTemplate } from '../../templates/index-page'
 
-const IndexPagePreview = ({ entry, getAsset }) => {
+const AboutPagePreview = ({ entry, widgetFor }) => {
   const data = entry.getIn(['data']).toJS()
 
   if (data) {
     return (
       <IndexPageTemplate
-        gallery={data.gallery || {}}
-        image={getAsset(data.image)}
+        content={widgetFor('body')}
+        about={data.about || {}}
       />
     )
   } else {
@@ -17,11 +17,11 @@ const IndexPagePreview = ({ entry, getAsset }) => {
   }
 }
 
-IndexPagePreview.propTypes = {
+AboutPagePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
-  getAsset: PropTypes.func,
+  widgetFor: PropTypes.func
 }
 
-export default IndexPagePreview
+export default AboutPagePreview
