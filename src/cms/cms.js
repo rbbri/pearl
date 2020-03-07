509e48f2dadabf45056c15c54227c7002bf49142
Robin Johnson <robin.s.johnson@gmail.com>
2020-03-07 12:24:16 +0000

    Update cms config

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -10,5 +10,5 @@ CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('brunch-menu', BrunchMenuPreview)
-CMS.registerPreviewTemplate('evening-menu', EveningMenuPreview )
+CMS.registerPreviewTemplate('brunch', BrunchMenuPreview)
+CMS.registerPreviewTemplate('evening', EveningMenuPreview )
